ff6fb2786ac7d31eaf865f23b3068c32468c6e2e
Hillevi Eklöw <hillevieklow@gmail.com>
2018-06-10 17:38:50 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -3,6 +3,11 @@ class Anagram
   attr_accessor :word
 
   def initialize(word)
-    
+    @word = word
   end
+
+  def match(possible_anagrams)
+    possible_anagrams.select{|current_word| current_word.chars.sort.join == @words.chars.sort.join}
+  end
+  
 end
